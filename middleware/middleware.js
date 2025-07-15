c0d5ec9dcf72154e92ff8792e2022a2f123c997e
toky <toky@callandco.fr>
2025-07-15 21:48:07 +0300

    rectif

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,7 +4,7 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.staus(401).json({message: "veuillez vous authentifier !"});
+    if (!token) return res.status(403).json({message: "veuillez vous authentifier !"});
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.status(401).json({message: "Token invalid ou éxpiré !"});
